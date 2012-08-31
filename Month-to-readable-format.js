418cda3eb2aabc812ab60852a3ae37f0aaacdc3d
evzen pomschar <rokker@seznam.cz>
2012-08-31 14:25:02 +0100

    month to readable format

---
+++ Month-to-readable-format.js
@@ -0,0 +1,14 @@
+var t = new Date(),
+	months = ['January','February','March','April','May','June','July','August','September','October','November','December' ],
+	month = months[t.getMonth()],
+	day = t.getDate(),
+	hours = t.getHours(),
+	minutes = t.getMinutes();
+	
+	// when day is single integer ad "0" before it
+	if(day <10) {
+		day = '0'+day;
+	}
+	
+	document.write(day + " " + month + " at " + hours + ":" + minutes);
+	
